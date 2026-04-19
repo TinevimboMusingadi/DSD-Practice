b296d0850a9c3740911d2fc3a0976c72b67f9edd
TinevimboMusingadi <tine2musi1@gmail.com>
2026-04-19 08:01:59 +0200

    added the stack.c

---
+++ prac2/stack.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int stack[256];
+int count = 0;
+
+void push(int x)
+{
+    stack[count]=x;
+    count++;
+}
+int pop()
+{
+    
+}
+
+int main()
+{
+    
+}
