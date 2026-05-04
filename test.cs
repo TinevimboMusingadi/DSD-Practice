bbd9ffb84b3805a365f6a730002d83d468263c29
TinevimboMusingadi <tine2musi1@gmail.com>
2026-05-04 23:35:37 +0200

    testing c# syntax

---
+++ test.cs
@@ -0,0 +1,15 @@
+using system 
+
+namespace HellowKing
+{
+    public class KingName
+    {
+
+        public static void Main(string[] args) //
+        {
+            Console.Writeline("king, wolrd!")
+        }
+         
+    }
+    
+}
