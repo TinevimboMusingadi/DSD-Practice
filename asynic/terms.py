e6a11a7afb600e2c7358f8bd7756133559805156
TinevimboMusingadi <tine2musi1@gmail.com>
2026-05-18 22:31:57 +0200

    started basic

---
+++ asynic/terms.py
@@ -0,0 +1,28 @@
+import asyncio 
+import time 
+
+def sync_function(test_param : str ) -> str : 
+	print("this is a synchronous function.")
+
+	time.sleep(0.1)
+
+	return f"sync results : {test_param}"
+
+# Also known as the a coroure funcinr 
+async def async_function(test_param: str) -> str :
+	print ("this is asynchronous coroutiine function")
+
+	await asyncio.sleep(0.2)
+
+	return f"Async results :{test_param}"
+
+async def main:
+	sync_result = sync_function("Test")
+
+	print(sync_result)
+
+
+
+if __name__ == "__main__":
+
+	asyncio.run(main())
